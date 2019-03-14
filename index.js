d825b85c1289ff8871873d9bd108d23e433262e2
Elise Phan <aliceconnic@gmail.com>
2019-03-14 03:56:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ function nameToAttributes(drivers) {
 
 function attributesToPhrase(drivers) {
   return drivers.map(function(driver) {
-    
+    return "${driver.name} is from ${driver.homwtown"
   });
-  
-  return "${driverName} is from ${driverHomeTown"
 }
 

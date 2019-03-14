d657a2a73a75113b9c9a68a6e114e0d93c592b8c
Elise Phan <aliceconnic@gmail.com>
2019-03-14 03:43:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function lowerCaseDrivers(drivers) {
 
 function nameToAttributes(drivers) {
   return drivers.map(function(driver) {
-    
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+
+    return { firstName: driverFirst, lastName: driverLast };
   })
 }
